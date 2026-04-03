6bb8a28c6c365a8fcceb229b09497ca73818ff8a
scottongart <ohmygot@gmail.com>
2026-04-03 10:46:59 +0700

    Add files via upload

---
+++ ws_server.js
@@ -0,0 +1,26 @@
+const WebSocket = require('ws');
+
+const port = process.env.PORT || 8080; // 🔥 สำคัญ
+const wss = new WebSocket.Server({ port });
+
+wss.on('connection', (ws) => {
+    console.log('Client connected');
+
+    ws.on('message', (message) => {
+        const data = JSON.parse(message);
+
+        if (data.store == 'Rannarak') {
+            wss.clients.forEach(client => {
+                if (client.readyState === WebSocket.OPEN) {
+                    client.send(JSON.stringify({
+                        store: data.store,
+                        type: data.type,
+                        table_no: data.table_no
+                    }));
+                }
+            });
+        }
+    });
+});
+
+console.log("WebSocket running on port", port);
